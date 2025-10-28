eecde86e531529ebf64c93abfedfd4e111bc5c97
Ramin Edjjal <ramin.edjjal135@gmail.com>
2025-10-29 01:26:43 +0330

    🔧 ساختار کاملاً جدید و تمیز - رفع خطاهای اساسی

--- functions/api/download.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestGet(context) {
-  return new Response(JSON.stringify({
-    message: "Use /api/download/[model_id] to download specific model"
-  }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
